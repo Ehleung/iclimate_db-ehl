f0f9d0baeb32a5c1580a05b13f1faef7cdb41cab
Ellery Leung <Ellery.h.leung@gmail.com>
2017-09-26 00:56:30 -0700

    Changed port from 443 to 80
    https -> http

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 443;
+var port = process.env.PORT || 80;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
@@ -59,5 +59,5 @@ require('./app/routes.js')(app, passport);	// load our routes and pass in our ap
 
 
 // Launch ==========================================================================
-app.listen(port, '18.221.138.216');
+app.listen(port);
 console.log('Server running at http://localhost:' + port);
