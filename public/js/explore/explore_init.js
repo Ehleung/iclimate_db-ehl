09eed7c795f15d5acac931a9cc90e4b86bfdd193
Ellery Leung <Ellery.h.leung@gmail.com>
2017-12-17 18:33:24 -0700

    change button to href location

--- public/js/explore/explore_init.js
+++ public/js/explore/explore_init.js
@@ -40,7 +40,7 @@ function world() {
   // Append every location found in the array at the top to a marker on the map
   for (var i = 0; i < locations.length; i++) {
     var marker = WE.marker(locations[i][1]).addTo(earth);
-    marker.bindPopup("<form action=\"/view\" method=\"post\"><h6>View stories from " + locations[i][0] + "!</h6></br> <button class=\"button is-primary\" name=\"viewLoc\" type=\"submit\">"+locations[i][0]+"</button></form>", {maxWidth: 320, closeButton: true});
+    marker.bindPopup("<form action=\"/view\" method=\"post\"><h6>View stories from <a href=\"#locations[i][0]\"></a>!</h6></br> <button class=\"button is-primary\" name=\"viewLoc\" type=\"submit\">"+locations[i][0]+"</button></form>", {maxWidth: 320, closeButton: true});
   }
 
   earth.switch_map = function switch_map(x)
