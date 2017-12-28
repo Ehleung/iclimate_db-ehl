ff04f7f2a49b91ae72c3bc0beeb43ea1d0818884
Ellery Leung <Ellery.h.leung@gmail.com>
2017-12-28 14:19:49 -0700

    replaced spaces with underscores in locations

--- public/js/explore/explore_init.js
+++ public/js/explore/explore_init.js
@@ -1,21 +1,21 @@
 var earth;
 
 var locations = [
-  ["Grand Canyon Nat. Park", [36.05972, -112.10972]],
-  ["Tonto Natural Bridge State Park", [34.32195, -111.45333]],
-  ["Organ Pipe Cactus National Monument ", [31.95556, -112.80028]],
-  ["Lake Havasu", [34.50306, -114.36028]],
+  ["Grand_Canyon_National_Park", [36.05972, -112.10972]],
+  ["Tonto_Natural_Bridge_State_Park", [34.32195, -111.45333]],
+  ["Organ_Pipe_Cactus_National_Monument ", [31.95556, -112.80028]],
+  ["Lake_Havasu", [34.50306, -114.36028]],
   ["Flagstaff", [35.14417, -111.66639]],
   ["Nogales", [31.42083, -110.84583]],
   ["Payson", [34.2325, -111.34473]],
   ["Phoenix", [33.42778, -112.00389]],
   ["Prescott", [34.57055, -112.43222]],
-  ["Window Rock", [35.6575, -109.06139]],
+  ["Window_Rock", [35.6575, -109.06139]],
   ["Yuma", [32.65,-114.61667]],
   ["Safford", [32.85472, -109.63528]],
   ["Tucson", [32.13139, -110.95528]],
   ["Tombstone", [31.71194, -110.06961]],
-  ["Tuzigoot National Monument", [34.77055, -112.02639]]
+  ["Tuzigoot_National_Monument", [34.77055, -112.02639]]
 ];
 
 function world() {
@@ -42,7 +42,7 @@ function world() {
     var marker = WE.marker(locations[i][1]).addTo(earth);
     marker.bindPopup("<h7><b>View stories from: </b></h7>" +
                       // "<br><h6>""</h6></br>" +
-                      "<a id=\"icon-"+locations[i][0]"\" onclick=\"getStories("+locations[i][0]+")\" >"+locations[i][0]+"</a>", {maxWidth: 320, closeButton: true});
+                      "<a id=\"icon-"+locations[i][0]+"\" onclick=\"getStories("+locations[i][0]+")\" >"+locations[i][0]+"</a>", {maxWidth: 320, closeButton: true});
   }//href=\"#modal"+locations[i][0]+"\"
 
   earth.switch_map = function switch_map(x)
