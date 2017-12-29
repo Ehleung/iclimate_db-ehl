e3bf08157d09674bb4d979f63aad2d02ae4c3c2f
Ellery Leung <Ellery.h.leung@gmail.com>
2017-12-28 20:15:19 -0700

    replaced quote with apostrophe

--- public/js/explore/explore_init.js
+++ public/js/explore/explore_init.js
@@ -42,7 +42,7 @@ function world() {
     var marker = WE.marker(locations[i][1]).addTo(earth);
     marker.bindPopup("<h7><b>View stories from: </b></h7>" +
                       // "<br><h6>""</h6></br>" +
-                      "<a id=\""+locations[i][0]+"\" href=\"#modal1"+"\" onclick=\"getStories(\""+locations[i][0]+"\")\">"+locations[i][0]+"</a>", {maxWidth: 320, closeButton: true});
+                      "<a id=\""+locations[i][0]+"\" href=\"#modal1"+"\" onclick=\"getStories(\'"+locations[i][0]+"\'')\">"+locations[i][0]+"</a>", {maxWidth: 320, closeButton: true});
   }//+locations[i][0]
   earth.switch_map = function switch_map(x)
    {
